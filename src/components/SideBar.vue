d927d6ba97b83448554ddf08dac0df8be1d78838
Binh <thanhbinh710@gmail.com>
2021-06-03 14:40:47 +0800

    setting up sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,27 +1,50 @@
 <template>
   <div class="stitcher-app-sidebar">
       <div class="logo-container">
-        <router-link to="/">
-          <img alt="Vue" src="../assets/logo.png">
+        <router-link class="sidebar-link" to="/">
+          <img class="sidebar-icon" alt="Vue" src="../assets/logo.png">
+          <div class="sidebar-label"></div>
         </router-link>
       </div>
-      <div class="sidebar-route">
-        <router-link to="/child/app-1">
-          <img alt="app-1" src="../assets/app-icon-1.png">
+      <div class="sidebar-route" v-for="app in appList" :key="app.id">
+        <router-link class="sidebar-link" :to="app.appUrl">
+          <img class="sidebar-icon" :alt="`app-${app.id}`" :src="getImgUrl(app.iconImage)">
+          <div class="sidebar-label">{{app.appName}}</div>
         </router-link>
       </div>
-      <div class="sidebar-route">
-        <router-link to="/child/app-2">
-          <img alt="app-2" src="../assets/app-icon-2.png">
+      <!-- <div class="sidebar-route">
+        <router-link class="sidebar-link" to="/child/app-2">
+          <img class="sidebar-icon" alt="app-2" src="../assets/app-icon-2.png">
+          <div class="sidebar-label">Child app 1</div>
         </router-link>
-      </div>
+      </div> -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'SideBar',
-  props: {
+  data: function () {
+    return {
+      appList: [{
+        id: 1,
+        iconImage: 'app-icon-1.png',
+        appName: 'Child app 1',
+        appUrl: '/child/app-1',
+        appDomain: 'http://localhost:8081'
+      },{
+        id: 2,
+        iconImage: 'app-icon-2.png',
+        appName: 'Child app 2',
+        appUrl: '/child/app-2',
+        appDomain: 'http://localhost:8082'
+      }]
+    }
+  },
+  methods: {
+    getImgUrl(pic) {
+      return require('../assets/'+pic)
+    }
   }
 }
 </script>
@@ -34,25 +57,65 @@ export default {
   height: 100%;
   background: #36495e;
   position: fixed;
-  padding: 16px 0;
+  padding: 24px 0;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
   z-index: 100;
+  transition: width 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
+  overflow: hidden;
+}
+.stitcher-app-sidebar:hover {
+  width: 200px;
+}
+
+.logo-container, .sidebar-route {
+  width: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
 }
 .logo-container {
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-bottom: 16px;
-  border-bottom: 1px solid white;
-}
-img {
-  width: 24px;
+  padding-bottom: 24px;
+  /* border-bottom: 1px solid white; */
 }
 .sidebar-route {
-  padding: 8px;
+  height: 64px;
+}
+.sidebar-route:hover {
+  background-color: #2B3A4B;
+}
+.sidebar-route:hover .sidebar-link {
+  color: #ffffff;
+}
+.sidebar-icon {
+  width: 32px;
+}
+.sidebar-label {
+  margin-left: 16px;
+}
+.sidebar-link {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #CCCCCC;
+  padding-left: 16px;
+  position: relative;
+}
+.router-link-active::before {
+  content: "";
+  position: absolute;
+  width: 5px;
+  height: 64px; 
+  background: #41B883;
+  left: 0;
+  top: -16px;
+}
+.router-link-active {
+  color: #ffffff;
+  font-weight: bold;
+  transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
 }
 
 </style>
